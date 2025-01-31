c5f8dd728ba4d187a11909b2ca057bec3196e0fc
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-31 05:57:17 -0500

    basic main boilerplate

---
+++ src/com/reggie/passwordgenerator/Main.java
@@ -0,0 +1,11 @@
+package com.reggie.passwordgenerator;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PasswordGUI gui = new PasswordGUI();
+            gui.setVisible(true);
+        });
+    }
+}
