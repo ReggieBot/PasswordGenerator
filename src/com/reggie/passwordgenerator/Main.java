56d31702b5973508711be69d13d839a64059f716
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-31 06:10:49 -0500

    PasswordGUI boilerplate methods created

--- src/com/reggie/passwordgenerator/Main.java
+++ src/com/reggie/passwordgenerator/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             PasswordGUI gui = new PasswordGUI();
-            gui.setVisible(true);
         });
     }
 }
--- src/com/reggie/passwordgenerator/PasswordGUI.java
+++ src/com/reggie/passwordgenerator/PasswordGUI.java
@@ -4,6 +4,29 @@ import javax.swing.JPanel;
 
 
 public class PasswordGUI {
-    // Basic frame
-    JFrame gui = new JFrame();
+    // Basic JFrame (window)
+    private JFrame gui;
+
+    // Constructor
+    public PasswordGUI() {
+        gui = new JFrame("Password Generator"); // Set title
+        gui.setSize(600, 400); // Set size
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close operation
+
+        setupGUI(); // Add components
+        setupActions(); // Add event listeners
+
+        gui.setVisible(true); // Show window
+    }
+
+    // GUI components
+    private void setupGUI() {
+        JPanel panel = new JPanel();
+        gui.add(panel);
+    }
+
+    // Event listeners
+    private void setupActions() {
+        
+    }
 }
