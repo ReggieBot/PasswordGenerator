ba5c2e6bba415354ea13d5f5a126142926c0a0a5
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-02-01 07:04:47 -0500

    Initial Flatlf setup

--- src/com/reggie/passwordgenerator/PasswordGUI.java
+++ src/com/reggie/passwordgenerator/PasswordGUI.java
@@ -14,4 +14,13 @@ public class PasswordGUI {
     private JTextField resultField;
     private JButton generateButton;
 
+    public PasswordGUI() {
+        // Set up Flatlaf
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize Flatlaf");
+        }
+
+    }
 }
